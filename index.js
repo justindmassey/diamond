16d85b68f061745524039786239c4dcc41cbc644
Justin Massey <justindmassey@gmail.com>
2026-03-02 15:14:05 +0100

    init

---
+++ index.js
@@ -0,0 +1,103 @@
+const cc = require("ccolor");
+const debug = false;
+if (!debug) {
+  const rl = require("./lib/rl")(cc.cyan("<> "), online);
+}
+
+class Node {
+  constructor(name = "") {
+    this.name = name;
+    this.children = [];
+  }
+
+  add(path) {
+    if (path.length) {
+      let found = false;
+      let rest = path.slice(1);
+      let added = false;
+      for (let child of this.children) {
+        if (child.name == path[0]) {
+          found = true;
+          if (child.add(rest)) {
+            added = true;
+          }
+        }
+      }
+      if (!found) {
+        let child = new Node(path[0]);
+        this.appendChild(child);
+        added = true;
+        child.add(rest);
+      }
+      return added;
+    }
+  }
+
+  find(name, result = []) {
+    for (let child of this.children) {
+      if (child.name == name) {
+        result.push(child);
+      }
+      child.find(name, result);
+    }
+    return result;
+  }
+
+  print(indent = 0) {
+    console.log(" ".repeat(indent) + this.name);
+    for (let child of this.children) {
+      child.print(indent + 4);
+    }
+  }
+  appendChild(child) {
+    this.children.push(child);
+    child.parent = this;
+  }
+
+  getPath(path = []) {
+    path.unshift(this.name);
+    if (this.parent) {
+      this.parent.getPath(path);
+    }
+    return path;
+  }
+}
+
+const root = new Node();
+
+function online(line) {
+  if (line.trim() == ",") {
+    for (let child of root.children) {
+      child.print();
+    }
+  } else {
+    let path = line.split(",").map((name) => name.trim());
+    let name = path[0];
+    if (!name) {
+      root.add(path.slice(1));
+    } else {
+      let rest = path.slice(1);
+      let nodes = root.find(name);
+      let added = false;
+      for (let node of nodes) {
+        if (node.add(rest)) {
+          added = true;
+        }
+      }
+      if (!added) {
+        for (let node of nodes) {
+          console.log(cc.magenta(node.getPath().slice(1).join(", ") + ":"));
+          node.print();
+          console.log();
+        }
+      }
+    }
+  }
+}
+
+if (debug) {
+  online(", user, justin, name");
+  online("name, vorname");
+  online("name, nachname");
+  online("name");
+}
